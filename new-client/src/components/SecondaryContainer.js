665fe4a09df82cef1d3df710ea15b77b80f94a2f
Isaac R <isaacreeder5@gmail.com>
2020-08-18 11:20:13 -0700

    side drawer begins

--- new-client/src/components/SecondaryContainer.js
+++ new-client/src/components/SecondaryContainer.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SecondaryContainer.css";
+import SideDrawer from "./SideDrawer";
 
 const SecondaryContainer = () => {
+  const [sideDrawerOpen, setSideDrawer] = useState(false);
   return (
     <div classname="titles">
       <h1 className="title-box">Types of products</h1>
       <h1 className="title-box">Current projects</h1>
       <h1 className="title-box">Members area</h1>
+      <SideDrawer
+        click={this.backdropClickHandler}
+        show={this.state.sideDrawerOpen}
+      />
     </div>
   );
 };
 
+drawerToggleClickHandler = () => {
+  this.setSideDrawer((prevState) => {
+    return { sideDrawerOpen: !prevState.setSideDrawer };
+  });
+};
+
+backdropClickHandler = () => {
+  this.setState({
+    sideDrawerOpen: false,
+  });
+};
 export default SecondaryContainer;
