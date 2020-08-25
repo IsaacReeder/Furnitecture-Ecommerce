2f03198e5eb9166d85de97414bfcb0b785c2eb24
Isaac R <isaacreeder5@gmail.com>
2020-08-25 10:18:08 -0700

    insterting hover triggered photo

--- new-client/src/components/LandingPage/NewLanding.js
+++ new-client/src/components/LandingPage/NewLanding.js
@@ -3,16 +3,36 @@ import React, { Component } from "react";
 import "./NewLanding.css";
 
 class NewLanding extends Component {
+  state = { imageSrc: "asdf" };
+  handleMouseEnter() {
+    this.setState({ imageSrc: "DummySRC" });
+  }
+  handleMouseExit() {
+    this.setState({ imageSrc: "DummySRC2" });
+  }
   render() {
     return (
       <div className="new-landing-container">
         <div className="navie">asdf</div>
         <div className="left-side">
-          asdf
-          <div className="paragraph"></div>
-          <div className="imgg"></div>
+          <div className="paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris
+          </div>
+          <div className="imgg">
+            <img src={this.state.imageSrc}></img>
+          </div>
         </div>
-        <div className="right-side">asdf</div>
+        <div className="right-side">
+          <h1
+            className="hover-title"
+            onMouseEnter={this.handleMouseEnter}
+            onMouseLeave={this.handleMouseExit}
+          >
+            E-commerce application
+          </h1>
+        </div>
       </div>
     );
   }
