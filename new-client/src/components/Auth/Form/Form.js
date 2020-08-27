5ca39c5ee6e51c06b1ff8cc4919e00f0404c8b65
Isaac R <isaacreeder5@gmail.com>
2020-08-27 16:26:45 -0700

    state not passing to child comp. after reset

--- new-client/src/components/Auth/Form/Form.js
+++ new-client/src/components/Auth/Form/Form.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
-// import { setToken } from "../utils";
+import { setToken } from "../../../utils/index";
 import Strapi from "strapi-sdk-javascript/build/main";
 import "./Form.css";
 const apiUrl = process.env.API_URL || "http://localhost:1337";
 const strapi = new Strapi(apiUrl);
 
 class Form extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-  state = {
-    username: "",
-    email: "",
-    password: "",
-    toast: false,
-    toastMessage: "",
-    loading: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      option: props.option,
+      username: "",
+      email: "",
+      password: "",
+      toast: false,
+      toastMessage: "",
+      loading: false,
+    };
+    console.log("within the constructor: " + this.state.option);
+  }
 
   handleChange = ({ event, value }) => {
     event.persist();
@@ -25,6 +27,7 @@ class Form extends Component {
 
   handleSubmit = async (event) => {
     event.preventDefault();
+    console.log(event, this.state);
     const { username, email, password } = this.state;
 
     if (this.isFormEmpty(this.state)) {
@@ -37,7 +40,7 @@ class Form extends Component {
       this.setState({ loading: true });
       const response = await strapi.register(username, email, password);
       this.setState({ loading: false });
-      //   setToken(response.jwt);
+      setToken(response.jwt);
       this.redirectUser("/");
     } catch (err) {
       this.setState({ loading: false });
@@ -60,6 +63,7 @@ class Form extends Component {
     // const { toastMessage, toast, loading } = this.state;
     // const [option, setOption] = React.useState(1);
     const option = this.state.option;
+    console.log(option);
     return (
       <form className="account-form" onSubmit={this.handleSubmit}>
         <div
@@ -69,38 +73,36 @@ class Form extends Component {
           }
         >
           <input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="E-mail"
+            id="username"
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={this.handleChange}
             required
-            onSubmit={this.handleSubmit}
+          />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            onChange={this.handleChange}
+            required
+            // required={option === 1 || option === 2 ? true : false}
+            // disabled={option === 3 ? true : false}
           />
           <input
             id="password"
+            type="password"
             name="password"
-            type="password"
             placeholder="Password"
-            required={option === 1 || option === 2 ? true : false}
-            disabled={option === 3 ? true : false}
-            onSubmit={this.handleSubmit}
-          />
-          <input
-            id="repeat-password"
-            name="repeat-password"
-            type="password"
-            placeholder="Repeat password"
-            required={option === 2 ? true : false}
-            disabled={option === 1 || option === 3 ? true : false}
-            onSubmit={this.handleSubmit}
+            onChange={this.handleChange}
+            required
+            // required={option === 2 ? true : false}
+            // disabled={option === 1 || option === 3 ? true : false}
           />
         </div>
         <button className="btn-submit-form" type="submit">
-          {option === 1
-            ? "Sign in"
-            : option === 2
-            ? "Sign up"
-            : "Reset password"}
+          {option === 1 ? "Sign in" : option === 2 ? "Sign up" : ""}
         </button>
       </form>
     );
