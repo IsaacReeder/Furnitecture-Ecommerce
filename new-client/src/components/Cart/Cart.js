121a5e122674f7bc53f8ab931c4d5cdada4cc24a
Isaac R <isaacreeder5@gmail.com>
2020-09-08 15:53:34 -0700

    cart modal renders products correctly

--- new-client/src/components/Cart/Cart.js
+++ new-client/src/components/Cart/Cart.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { setCart, getCart, calculatePrice } from "../../utils/index";
 import "./Cart.css";
 
+const apiUrl = process.env.API_URL || "http://localhost:1337";
+// const strapi = new Strapi(apiUrl);
+
 class Cart extends Component {
   state = {
     cartItems: getCart(),
@@ -18,26 +21,31 @@ class Cart extends Component {
         <div className="products-cart">
           <div className="cart-left-section">
             Bag
-            <h4>Items: {this.state.cartItems.length}</h4>
+            {/* <h4>Items: {this.state.cartItems.length}</h4> */}
             {this.state.cartItems.map((product) => (
-              <div key={product.id}>
+              <>
+                <img
+                  style={{ width: "50%" }}
+                  src={`${apiUrl}${product.image[0].url}`}
+                  alt="item pic"
+                ></img>
                 <div className="product-name">{product.name}</div>
                 <div className="product-price">
                   Quantity {product.quantity} - $
                   {(product.quantity * product.price).toFixed(2)}
                 </div>
-                <h3>
+                <h3 style={{ borderBottom: "1px solid black" }}>
                   <div onClick={() => this.deleteItemFromCart(product.id)}>
                     <h4>Remove</h4>
                   </div>
                 </h3>
                 <br />
-                <h3>
+                {/* <h3>
                   {this.state.cartItems.length === 0 && (
                     <h3>Please select some items</h3>
                   )}
-                </h3>
-              </div>
+                </h3> */}
+              </>
             ))}
           </div>
           <div className="cart-right-section">
