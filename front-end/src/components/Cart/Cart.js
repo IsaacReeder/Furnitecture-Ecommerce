efbc77b5698fec102a387ce17549d7fce77be63a
Isaac R <isaacreeder5@gmail.com>
2020-11-12 14:35:40 -0800

    cart styles

--- front-end/src/components/Cart/Cart.js
+++ front-end/src/components/Cart/Cart.js
@@ -5,7 +5,7 @@ import {
   calculatePrice,
   calculateShipping,
 } from "../../utils/index";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Cart.css";
 
 const apiUrl = process.env.API_URL || "http://localhost:1337";
@@ -35,10 +35,120 @@ class Cart extends Component {
   //     });
   // }
   render() {
+    const { cartItems } = this.state;
     return (
-      <>
+      <div
+        style={{
+          display: "flex",
+          padding: "5%",
+          flexDirection: "column",
+          height: "100vh",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            paddingTop: "10%",
+            flex: "1",
+          }}
+        >
+          <div style={{ fontSize: "3vw" }}>Your shopping cart</div>
+        </div>
         <div className="products-cart">
-          <div className="cart-left-section">
+          {cartItems.map((product) => (
+            <div
+              style={{
+                display: "flex",
+                border: ".5px solid rgb(202, 201, 201)",
+                marginBottom: "10px",
+              }}
+            >
+              <div
+                style={{
+                  flex: "0 32%",
+                  height: "100%",
+                }}
+              >
+                <img
+                  style={{ width: "110%" }}
+                  src={`${apiUrl}${product.image[0].url}`}
+                  alt="item pic"
+                ></img>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flex: "0 32%",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "flex-start",
+                  height: "100px",
+                  alignContent: "space-between",
+                  paddingTop: "5%",
+                  paddingLeft: "5%",
+                }}
+              >
+                <div style={{ flex: "1", fontSize: "2vw" }}>{product.name}</div>
+
+                <div style={{ flex: "1", fontSize: "1.2vw" }}>
+                  {product.description}
+                </div>
+                <div style={{ flex: "1", fontSize: "1vw" }}>
+                  Quantity: {product.quantity}
+                </div>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flex: "0 32%",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100px",
+                  paddingTop: "5%",
+                }}
+              >
+                <i
+                  className="fas fa-trash"
+                  style={{ fontSize: "30px", cursor: "pointer", flex: "1" }}
+                  onClick={() => this.deleteItemFromCart(product.id)}
+                ></i>
+                <div style={{}}>
+                  $ {(product.quantity * product.price).toFixed(2)}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div style={{ fontSize: "1.5vw" }}>
+          Subtotal: {calculatePrice(cartItems)}
+        </div>
+        <Link
+          to={`/checkout`}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "50px",
+            backgroundColor: "black",
+            color: "white",
+            background: "black",
+            textDecoration: "none",
+            marginBottom: "10%",
+            marginTop: "5%",
+            flex: "1",
+            zIndex: "1000",
+          }}
+        >
+          Checkout
+        </Link>
+      </div>
+    );
+  }
+}
+{
+  /* <div className="cart-left-section">
             {this.state.cartItems.map((product) => (
               <>
                 <img
@@ -84,11 +194,9 @@ class Cart extends Component {
               <Link to={`/checkout`}>
                 <h1>Checkout</h1>
               </Link>
-            </button> */}
-          </div>
-        </div>
-      </>
-    );
-  }
+            </button> */
 }
+
+// </div> */}
+
 export default Cart;
